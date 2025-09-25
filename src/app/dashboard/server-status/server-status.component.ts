6ee4d2e502ba21534a0a35c3f116cbd5081007f1
Peyno <75258235+Peyno@users.noreply.github.com>
2025-09-25 13:59:38 +0200

    finishing Video 140

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, effect, inject, OnDestroy, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,8 +7,42 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
-    currentStatus = 'offline';
+export class ServerStatusComponent implements OnInit {
+  currentStatus = signal<'online' | 'offline' | 'unknown'>('offline');
+  private destroyRef = inject(DestroyRef);
+
+  constructor() {
+    effect(() => {
+      console.log(this.currentStatus());
+    }); 
+  }
+
+  ngOnInit() {
+    console.log('ON INIT');
+    const interval = setInterval(() => {
+      const rnd = Math.random(); // 0 - 0.9999999999
+
+      if (rnd < 0.5) {
+        this.currentStatus.set('online');
+      } else if (rnd < 0.9) {
+        this.currentStatus.set('offline');
+      } else {
+        this.currentStatus.set('unknown');
+      }
+    }, 5000);
+
+    this.destroyRef.onDestroy(() => {
+      clearInterval(interval);
+    })
+  }
+
+  ngAfterViewInit() {
+    console.log('AFTER VIEW INIT');
+  }
+
+  // ngOnDestroy() {
+  //     clearTimeout(this.interval); 
+  // }
 
 
 }
--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, HostBinding, HostListener, inject, input, ViewEncapsulation } from '@angular/core';
+import { AfterContentInit, afterNextRender, afterRender, Component, contentChild, ContentChild, ElementRef, HostBinding, HostListener, inject, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -12,17 +12,36 @@ import { Component, ElementRef, HostBinding, HostListener, inject, input, ViewEn
      '(click)': 'onClick()'
   }
 })
-export class ControlComponent {
+export class ControlComponent implements AfterContentInit {
   // @HostBinding('class') className = 'control'; 
 //  @HostListener('click') onClick(){
 //     console.log('Clicked!'); 
 //   }
   label = input.required<string>(); 
   private el = inject(ElementRef); 
+  // @ContentChild('input') private control?: ElementRef<
+  // HTMLInputElement | HTMLTextAreaElement
+  // >; 
+  private control = contentChild<ElementRef<HTMLInputElement | HTMLTextAreaElement>>('input'); 
+
+  constructor() {
+    afterRender(() => {
+      console.log('afterRender'); 
+    }); 
+
+    afterNextRender(() => {
+      console.log('afterNextRender'); 
+    }); 
+  }
+
+  ngAfterContentInit() {
+      //...
+  }
 
   onClick(){
     console.log('Clicked!');
     console.log(this.el);  
+    console.log(this.control()); 
   }
 
 }
